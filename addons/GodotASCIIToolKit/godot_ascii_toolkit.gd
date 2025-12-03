e228c8779b68634ad3668878055bf764868086c9
Vost <vost_public@proton.me>
2025-12-04 00:01:12 +0100

    Actually removing autoload.

--- addons/GodotASCIIToolKit/godot_ascii_toolkit.gd
+++ addons/GodotASCIIToolKit/godot_ascii_toolkit.gd
@@ -23,14 +23,12 @@ extends EditorPlugin
 
 func _enable_plugin() -> void:
 	# Add autoloads here.
-	add_autoload_singleton(
-		"ASCIISettings", "ascii_settings.gd"
-	)
+	pass
 
 
 func _disable_plugin() -> void:
 	# Remove autoloads here.
-	remove_autoload_singleton("ASCIISettings")
+	pass
 
 
 func _enter_tree() -> void:
